6ca9395a0c066708acd2a7e826e98efe6a2bef21
naglaswangas <fernandosolisjr@gmail.com>
2019-10-29 19:29:53 -0500

    Start button working and displaying questions; correct answers showing in log

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,15 +1,15 @@
 $(document).ready(function () {
-
+    $("#questions").hide();
 var questions =[
     {
         quest: "What is dwights cousin's name?",
         choice: ["Ted", "Mose", "Joe", "Matthew"],
-        ans: 1,
+        ans: "Mose",
     },
     {
         quest: "What university did Andy go to?",
         choice: ["Cornell", "UTSA", "Yale", "Harvard"],
-        ans: 0,
+        ans: "Cornell",
     },
     {
         quest: "Toby broke his back in ______ ?",
@@ -17,33 +17,65 @@ var questions =[
         ans: 3
     }];
 
+    // $("input[name='Q1']:checked").val();
 
 var correct = 0;
 var wrong = 0;
-var timer = 60;
+var time = 60;
 var qlist = questions.length;
+var questArray = [];
+var running = false;
 
 
 // reset //
-
-
-// Timer //
-
-function runTimer(){
+function timer(){
 	if (!running) {
-	intervalId = setInterval(decrement, 1000); 
+	intervalId = setInterval(countDown, 1000); 
 	running = true;
 	}
 }
+function result_count(){
+    var a = [$("input[name='q1']:checked").val(),$("input[name='q2']:checked").val()];
+    console.log("A array:"+a);
+    for(var i=0;i<a.length;i++){
+        if(a[i]===questions[i].ans){
+            correct++;
+            console.log("Correct: "+correct);
+        }
+        else{
+            wrong++;
+        }
+    }
+}
+// start button //
+$("#ans").on("click", function(){
+   result_count();
+})
+
+$("#start").on("click",function() {
+    $("#start").hide();
+    $("#questions").show();
+    //showQuests();
+    timer();
+    /*for (let i = 0; i < questions.length; i++) {
+        questArray.push (questions[i]);
+        }*/
+})
+
+
+// start timer //
+
+
 // count down //
-function decrement() {
-	$("#timeleft").html("Time remaining: " + timer + "");
-	timer --;
+function countDown() {
+	$("#timeleft").html("Time remaining: " + time);
+	time --;
 
 //stop timer if reach 0
-	if (timer === 0) {
-		unanswerCount++;
-		stop();
+	if (time === 0) {
+		//unanswerCount++;
+        stop();
+        result_count();
 	}	
 }
 
@@ -53,8 +85,7 @@ function stop() {
 	clearInterval(intervalId);
 
 
-// functions
-
+}
 
 
 
